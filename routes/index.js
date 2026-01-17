5a573af046cbeb554af1103f813c5feb6cc23bb9
Bart Dority <b0rgbart3@gmail.com>
2026-01-17 14:16:58 -0800

    adding lightbox project demo

--- routes/index.js
+++ routes/index.js
@@ -4,37 +4,33 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const axios = require("axios");
 
-// API Routes for data pertaining to our DB
+
+// 🔹 Serve Flutter static files
+// router.use(
+//   "/lightburst",
+//   express.static(path.join(__dirname, "../public/lightburst"))
+// );
+
+// router.get("/lightburst", (req, res) => {
+//   console.log("Got to lightburst.");
+
+//   res.sendFile(
+//     path.join(__dirname, "../public/lightburst/index.html")
+//   );
+// });
+
+// router.get("/lightburst/*", (req, res) => {
+//   console.log("Got to a lightburst subpage.");
+
+//   res.sendFile(
+//     path.join(__dirname, "../public/lightburst/index.html")
+//   );
+// });
+
+
+// API Routes 
 router.use("/api", apiRoutes);
 
-
-// // API Route to query the Google API
-// router.get("/google/:name", (req, res) => {
-//   // make an api call to `https://www.googleapis.com/books/v1/volumes?q=<Book Name>` and return the relevant results.
-
-//   console.log("Looking for: "+ req.params.name  );
-//    //res.json({q: req.params.name });
-
-//   axios.get("https://www.googleapis.com/books/v1/volumes",
-//    { params: { q: req.params.name }}).then(({ data }) => {
-
-//     console.log(data.items);
-
-//     res.json( data.items );
-//   })
-//     .catch(err => res.status(422).json(err));
-// });
-
-// Jerome's example
-// router.get("/recipes", (req, res) => {
-//   axios
-//     .get("http://www.recipepuppy.com/api/", { params: req.query })
-//     .then(({ data: { results } }) => res.json(results))
-//     .catch(err => res.status(422).json(err));
-// });
-
-// Send every other request to the React app
-// Define any API routes before this runs
 
 router.get("/BART_DORITY_resume.pdf", (req, res) => {
   console.log("serving up the static pdf.");
@@ -46,6 +42,8 @@ router.get("*", (req, res) => {
 
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,50 +1,27 @@
 const express = require("express");
-// const path = require("path");
-// const PORT = process.env.PORT || 3002;
+const path = require("path");
 const app = express();
-// const mongoose = require("mongoose");
-const routes = require("./routes");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// app.use(express.json({ limit: '1mb' }));
-// app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-
-app.use('/googlebooks', createProxyMiddleware({
-  target: 'http://localhost:8001',
-  changeOrigin: true,
-}));
-
-app.use((req, res, next) => {
-  console.log("Incoming request:", req.method, req.url);
-  next();
-});
-
-// all routes are in here
-// app.use((req, res, next) => {
-//   console.log("Headers received:", req.headers);
-//   const cookieHeader = req.headers['cookie'] || '';
-//   if (cookieHeader.length > 4000) {
-//     return res.status(431).send("Too many cookies");
-//   }
-//   next();
-// });
-
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-// }
 
 const PORT = 8000;
 
-// all routes are in here
-app.use(routes);
+app.use(express.json());
 
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// 🔥 Serve public folder FIRST
+app.use(express.static(path.join(__dirname, "public")));
+
+// 🔥 Flutter entry point
+app.get("/lightburst", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/lightburst/index.html"));
+});
+
+// 🔥 Flutter deep links
+app.get("/lightburst/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/lightburst/index.html"));
+});
+
+// API routes AFTER static
+const routes = require("./routes");
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
