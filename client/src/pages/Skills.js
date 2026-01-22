a4e5a437e43f9a1773464161c73c40ac6ee1961c
Bart Dority <b0rgbart3@gmail.com>
2026-01-22 15:37:02 -0800

    finish migration of skills page

--- client/src/pages/Skills.js
+++ client/src/pages/Skills.js
@@ -1,222 +1,288 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Skills.scss";
 import { Link } from "react-router-dom";
 import Page from "../components/Page";
 import Logo from "../components/Logo";
-import pointerIcon from '../images/icons/white_arrow.svg';
-
+import pointerIcon from "../images/icons/white_arrow.svg";
 
 function Skills() {
   const [chartStyle, setChartStyle] = useState("chart");
   const techObjects = [
-
     {
-    "tech": "React",
-    "shield": "react",
-    "duration": "5 years",
-    "projects": "Grid Dynamics"
-    },
+      category: "Front-End & UI Engineering",
+      skills: [
         {
-    "tech": "TypeScript",
-    "shield": "ts",
-    "duration": "4 years",
-    "projects": "Grid Dynamics"
-    },
-            {
-      "tech": "Javascript",
-      "shield": "js",
-      "duration": "7+ years",
-      "projects": "All"
-      },
+          tech: "React",
+          shield: "react",
+        },
         {
-    "tech": "NodeJS",
-    "shield": "node",
-    "duration": "3 years",
-    "projects": "Grid Dynamics"
-    },
+          tech: "TypeScript",
+          shield: "ts",
+        },
         {
-    "tech": "Express",
-    "shield": "express",
-    "duration": "2 years",
-    "projects": "Grid Dynamics"
-    },
-              {
-      "tech": "Material UI",
-      "shield": "mui",
-      "duration": "2 years",
-      "projects": "Grid Dynamics"   
-  },
-      {
-        "tech": "HTML 5",
-        "shield": "html",
-        "duration": "10+ years",
-        "projects": "All"  
-    },
-    {
-        "tech": "CSS 3",
-        "shield": "css",
-        "duration": "7+ years",
-        "projects": "All"   
-    },
+          tech: "Javascript",
+          shield: "js",
+        },
 
-    {
-      "tech": "Sass",
-      "shield": "sass",
-      "duration": "2 years",
-      "projects": "This portfolio, My Google Books"   
-  },
-
-    {
-      "tech": "Redux",
-      "shield": "redux",
-      "duration": "1 project",
-      "projects": "SpaceForce"
-    },    
-
-      {
-        "tech": "Flutter",
-        "shield": "flutter",
-        "duration": "3 months",
-        "projects": "Light Burst 2.0"  
+        {
+          tech: "Material UI",
+          shield: "mui",
+        },
+        {
+          tech: "HTML 5",
+          shield: "html",
+        },
+        {
+          tech: "CSS 3",
+          shield: "css",
+        },
+        {
+          tech: "Sass",
+          shield: "sass",
+        },
+        {
+          tech: "MobX",
+          shield: "mobx",
+        },
+        {
+          tech: "Redux",
+          shield: "redux",
+        },
+      ],
     },
     {
-        "tech": "Dart",
-        "shield": "dart",
-        "duration": "3 months",
-        "projects": "Light Burst 2.0"   
+      category: "Design & Prototyping",
+      skills: [
+        {
+          tech: "Figma",
+          shield: "figma",
+        },
+        {
+          tech: "Adobe",
+          shield: "adobe",
+        },
+      ],
     },
     {
-      "tech": "Python3+",
-      "shield": "python",
-      "duration": "3 months",
-      "projects": "Leet Code Drills"   
-  },
-
-
-    {
-    "tech": "Mongo",
-    "shield": "mongo",
-    "duration": "2 years",
-    "projects": "My Google Bookshelf, The Loom, Name List, Memory Lane"
+      category: "AI-Assisted Development",
+      skills: [
+        {
+          tech: "Chat GPT",
+          shield: "chat",
+        },
+        {
+          tech: "Claude",
+          shield: "claude",
+        },
+        {
+          tech: "Gemini",
+          shield: "gemini",
+        },
+        {
+          tech: "Perplexity",
+          shield: "perplexity",
+        },
+        {
+          tech: "Grok",
+          shield: "grok",
+        },
+      ],
     },
     {
-    "tech": "Mongoose",
-    "shield": "mongoose",
-    "duration": "1 year",
-    "projects": "My Google Bookshelf, Name List, Memory Lane"
+      category: "Back-End & API Integration",
+      skills: [
+        {
+          tech: "NodeJS",
+          shield: "node",
+        },
+        {
+          tech: "Express",
+          shield: "express",
+        },
+        {
+          tech: "Mongoose",
+          shield: "mongoose",
+        },
+      ],
     },
     {
-        "tech": "MySQL",
-        "shield": "sql",
-        "duration": "5 years",
-        "projects": "ImageShoppr, The Loom, MiddleGround, Suppr, JerryFrost.com"
+      category: "Databases",
+      skills: [
+        {
+          tech: "Mongo",
+          shield: "mongo",
+        },
+        {
+          tech: "MySQL",
+          shield: "sql",
+        },
+        {
+          tech: "PostgreSQL",
+          shield: "postgre",
+        },
+      ],
     },
     {
-        "tech": "Angular2+",
-        "shield": "angular",
-        "duration": "2 years",
-        "projects": "The Loom" 
+      category: "Testing & Quality",
+      skills: [
+        {
+          tech: "Jest with Enzyme",
+          shield: "jest",
+        },
+      ],
     },
     {
-        "tech": "Objective C",
-        "shield": "objc",
-        "duration": "2 years",
-        "projects": "Light Burst"   
+      category: "DevOps & Tooling",
+      skills: [
+        {
+          tech: "GIT",
+          shield: "git",
+        },
+        {
+          tech: "GitHub",
+          shield: "github",
+        },
+        {
+          tech: "AWS",
+          shield: "aws",
+        },
+        {
+          tech: "GitLab",
+          shield: "gitlab",
+        },
+        {
+          tech: "Jenkins",
+          shield: "jenkins",
+        },
+        {
+          tech: "Heroku",
+          shield: "heroku",
+        },
+        {
+          tech: "VSCode",
+          shield: "vscode",
+        },
+      ],
     },
     {
-        "tech": "PHP",
-        "shield": "php",
-        "duration": "7 years",
-        "projects": "JerryFrost.com, DDWorks.org"   
+      category: "Additional Experience",
+      skills: [
+        {
+          tech: "Flutter",
+          shield: "flutter",
+        },
+        {
+          tech: "Dart",
+          shield: "dart",
+        },
+        {
+          tech: "Python3+",
+          shield: "python",
+        },
+        {
+          tech: "Angular2+",
+          shield: "angular",
+          duration: "2 years",
+          projects: "The Loom",
+        },
+        {
+          tech: "Objective C",
+          shield: "objc",
+          duration: "2 years",
+          projects: "Light Burst",
+        },
+        {
+          tech: "PHP",
+          shield: "php",
+          duration: "7 years",
+          projects: "JerryFrost.com, DDWorks.org",
+        },
+        {
+          tech: "Ruby on Rails",
+          shield: "ruby",
+          duration: "2 years",
+          projects: "experimental projects",
+        },
+        {
+          tech: "WordPress ",
+          shield: "wordpress",
+          duration: "4 years",
+          projects: "several client websites",
+        },
+      ],
     },
-    {
-      "tech": "Ruby on Rails",
-      "shield": "ruby",
-      "duration": "2 years",
-      "projects": "experimental projects"   
-    },
-    {
-      "tech": "WordPress ",
-      "shield": "wordpress",
-      "duration": "4 years",
-      "projects": "several client websites"   
-    }
-
   ];
 
   useEffect(() => {
-
     let chartTimer = setTimeout(function () {
       setChartStyle("chart chart2");
       clearTimeout(chartTimer);
-    },200);
+    }, 200);
   });
-
 
   function pageContent() {
     return (
-
       <>
-     <Link to="/">
-      <Logo stacked="false"/>
-     </Link>
-<br />
-     <Link to="/endorsements">
-        <div className="mbuttonEndorse"><img src={pointerIcon}/>read my endorsements</div>
-      </Link>
+        <Link to="/">
+          <Logo stacked="false" />
+        </Link>
+        <br />
+        <Link to="/endorsements">
+          <div className="mbuttonEndorse">
+            <img src={pointerIcon} />
+            read my endorsements
+          </div>
+        </Link>
 
-      <div className='title'>Skills</div>
-      <ul><li>User Interface Design</li>
-       <li>Web Design</li>
-      <li>Web Development and Hosting</li>
-      <li>Mobile Development</li>
-      <li>Deisgn and Engineering Mentorship</li>
-        </ul>
-      <p>I focus on front-end user interface web development.   My core skills include Typescript, Javascript, React, and NodeJS.
-        
-      </p>
-      <p>I'm always learning new technologies and new tech-platforms.  Here are some of the popular web development technologies that I have experice with:</p>
-      <div className='title'>Technologies</div>
+        <div className="title">Skills</div>
 
-      <div className={chartStyle}>
-      <div className="tech chartLabels">
-        <div className='techShield'></div>
-        <div className='techName label'>Technology</div>
-        <div className='techYears label'>Experience</div>
-        <div className='techYears label'>Projects</div>
-      </div>
-      {techObjects.map((techObj, index) => (
-       <div className={index%2===0 ? "tech" : "tech alt" } key={index}>
-       
-        <div className="techShield">
-        <img
-                  className="techShieldIcon"
-                  src={require("../images/shields/" +
-                    techObj["shield"] +
-                    ".png")} alt={techObj["tech"]} />
-                    {/* <Shield shieldNumber={index} animate="false" /> */}
-                    </div>
-        <div className="techName">{techObj["tech"]}</div>
-        <div className="techYears">{techObj["duration"]}</div>
-        <div className="techProject">{techObj["projects"]}</div>
+        <p className="pageContent">
+          I specialize in front-end UI engineering with full-stack experience,
+          focusing on building scalable, user-centered web applications. My core
+          strengths include React, TypeScript, JavaScript, and Node.js, with an
+          emphasis on maintainable architecture, API-driven systems, and modern
+          development workflows.
+        </p>
+        {/* <p>
+          This page expands on the skills listed in my resume by showing how and
+          where I apply them in real projects.
+        </p> */}
+        <p className="pageContent">
+          Much of my recent professional work has been on proprietary enterprise
+          applications and is not publicly accessible. Descriptions here focus
+          on the systems, responsibilities, and technologies involved.
+        </p>
 
-       </div>
-        
-      ))}
-      </div>
-    </>
-
-    )
+        <div className={chartStyle}>
+          {techObjects.map((techObj, index) => (
+            <div className="techCategory">
+              <p className="skillCategory">{techObj.category}</p>
+              {techObj.skills.map((techSkill, index) => (
+                <div className="tech" key={index}>
+                  <div className="techShield">
+                    <img
+                      className="techShieldIcon"
+                      src={require(
+                        "../images/shields/" + techSkill["shield"] + ".png",
+                      )}
+                      alt={techSkill["tech"]}
+                    />
+                  </div>
+                  <div className="techName">{techSkill["tech"]}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>
+    );
   }
   return (
     <div className="verticalExtender">
-    <Page content={pageContent()} />
-   </div>
-
-    
+      <Page content={pageContent()} />
+    </div>
   );
 }
 
 export default Skills;
-
